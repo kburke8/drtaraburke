5700855cb733c8d759a3ab64bf75751d5be45e2b
Kyle Burke <burke.k@outlook.com>
2021-01-18 16:12:27 -0700

    use hook

--- _plugins/hooks/purgecss.rb
+++ _plugins/hooks/purgecss.rb
@@ -9,6 +9,6 @@ Jekyll::Hooks.register(:site, :post_write) do |_site|
     # system("purgecss --config #{config_file} --output _site/assets")
     #/github/workspace/_plugins/hooks  
     # File.chmod(0777,"/github/workspace/node_modules/.bin/purgecss")
-    system("/github/workspace/node_modules/.bin/purgecss --css _site/assets/css/bootstrap.min.css --content _site/**/*.html --safelist bg-light --output _site/assets/css")
+    system("purgecss --css _site/assets/css/bootstrap.min.css --content _site/**/*.html --safelist bg-light --output _site/assets/css")
     #system("/github/workspace/node_modules/.bin/purgecss --css _site/assets/css/bootstrap.min.css --content _site/**/*.html --safelist bg-light --output _site/assets/css")
   end
