afa6707592c237ced87a2f82312fee1439180fae
kburke8 <kburke8@users.noreply.github.com>
2024-07-05 19:39:26 +0000

    actual fix 5fd759e133466821396e152cf2a8429ea96cfd3e

--- assets/js/contact_me.js
+++ assets/js/contact_me.js
@@ -8,7 +8,7 @@ $(function() {
     submitSuccess: function($form, event) {
       event.preventDefault(); // prevent default submit behaviour
       // get values from FORM
-	  var url = "https://formspree.io/" + "moqpykav";
+	  var url = "https://formspree.io/f/moqpykav";
       var name = $("input#name").val();
       var email = $("input#email").val();
       var phone = $("input#phone").val();
